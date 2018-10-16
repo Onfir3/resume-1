ad1fe476a8c63660dccc46e58f057193679de8ab
Onfir3 <w526529650@outlook.com>
2018-10-16 23:44:43 +0800

    sticky.js增加MVC

--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -4,11 +4,30 @@ setTimeout(function () {
 
 
 !function(){
-    window.addEventListener('scroll',function(xxx){
-        if (window.scrollY > 0) {
-            TopNavBar.classList.add('sticky')
-        } else {
-            TopNavBar.classList.remove('sticky')
+    var view = document.querySelector('#TopNavBar')
+
+    var controller = {
+        view:null,
+        init: function(view){
+            this.view = view
+            this.bindEvents()
+        },
+        bindEvents: function(){
+            var view = this.view
+            window.addEventListener('scroll',(xxx) =>{
+                if (window.scrollY > 0) {
+                    this.active()
+                } else {
+                    this.deactive()
+                }
+            })
+        },
+        active: function(){
+            this.view.classList.add('sticky')
+        },
+        deactive: function(){
+            this.view.classList.remove('sticky')
         }
-    })
+    }
+    controller.init(view)
 }.call()
