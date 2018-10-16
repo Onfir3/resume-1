1fe4a135aea9b58a9499456e504f1dcf3d933b78
Onfir3 <w526529650@outlook.com>
2018-10-17 00:55:44 +0800

    增加MVC

--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,21 +1,19 @@
-!function(){
-    let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-    function animate(time) {
-        requestAnimationFrame(animate);
-        TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
-    
-    for (let i = 0; i < aTags.length; i++) {
-        aTags[i].onclick = function (x) {
-            x.preventDefault()//阻止默认动作
-            let a = x.currentTarget //获取a标签
-            let herf = a.getAttribute('href') //获取a标签上的herf
-            let element = document.querySelector(herf)//根据herf获取到元素
+!function () {
+    var view = document.querySelector('nav.menu')
+    var controller = {
+        view: null,
+        aTags: null,
+        initAnimation: function () {
+            function animate(time) {
+                requestAnimationFrame(animate);
+                TWEEN.update(time);
+            }
+            requestAnimationFrame(animate);
+        },
+        scrollToElement: function (element) {
             let top = element.offsetTop//根据element.offsetTop得到元素到窗口顶端的距离
             //let top = document.querySelector(x.currentTarget.getAttribute('href')).offsetTop //以上四句简写.
             //window.scrollTo(0, top - 80)//让window滑动到X方向不变，Y方向top-80的距离
-    
             let currentTop = window.scrollY
             let targetTop = top - 80
             let s = targetTop - currentTop //路程
@@ -30,6 +28,24 @@
                     window.scrollTo(0, coords.y) // 如何更新界面
                 })
                 .start(); //开始缓动
+        },
+        bindEvents: function () {
+            let aTags = this.view.querySelectorAll('nav.menu > ul > li > a')
+            for (let i = 0; i < aTags.length; i++) {
+                aTags[i].onclick = (x) => {
+                    x.preventDefault()//阻止默认动作
+                    let a = x.currentTarget //获取a标签
+                    let herf = a.getAttribute('href') //获取a标签上的herf
+                    let element = document.querySelector(herf)//根据herf获取到元素 
+                    this.scrollToElement(element)
+                }
+            }
+        },
+        init: function () {
+            this.view = view
+            this.initAnimation()
+            this.bindEvents()
         }
     }
+    controller.init(view)
 }.call()
