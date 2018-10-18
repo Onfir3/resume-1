53a28dfebae53b0ad3ddfbb4ddb7b38f9fc8bba1
Onfir3 <w526529650@outlook.com>
2018-10-18 23:21:26 +0800

    返回留言在页面上显示

--- js/message.js
+++ js/message.js
@@ -1,24 +1,36 @@
 var APP_ID = 'lVd3kDdlL79TmArs9hnvudYP-gzGzoHsz';
 var APP_KEY = 'Hpan2b16tVNcSfr4pWweNmqB';
-console.log('1')
 AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
+    appId: APP_ID,
+    appKey: APP_KEY
 });
-console.log('2')
+
+var query = new AV.Query('message');
+query.find()
+    .then(
+        function (messages) {
+            let array = messages.map((item) => item.attributes)
+            console.log(array)
+            array.forEach((item) => {
+                let li = document.createElement('li')
+                li.innerText = item.content
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+            })
+        })
 
 let myForm = document.querySelector('#postMessageForm')
 
-myForm.addEventListener('submit',function(e){
+myForm.addEventListener('submit', function (e) {
     e.preventDefault()
     let content = myForm.querySelector('input[name=content]').value
     var message = AV.Object.extend('message');
     var message = new message();
     message.save({
-    content: content
-    }).then(function(object) {
-    alert('LeanCloud Rocks!');
-})
+        content: content
+    }).then(function (object) {
+        window.location.reload()
+    })
 })
 
 
