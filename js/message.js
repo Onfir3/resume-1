8f71706935200aa6aed47f7ed3db6f52079b75ec
Onfir3 <w526529650@outlook.com>
2018-10-19 18:14:52 +0800

    解决一个bug

--- js/message.js
+++ js/message.js
@@ -6,11 +6,11 @@
             var APP_KEY = 'Hpan2b16tVNcSfr4pWweNmqB';
             AV.init({ appId: APP_ID, appKey: APP_KEY })
         },
-        fetch: function(){
+        fetch: function () {
             var query = new AV.Query('message');
             return query.find()
         },
-        save: function(name,content){
+        save: function (name, content) {
             var message = AV.Object.extend('message');
             var message = new message();
             return message.save({
@@ -23,47 +23,50 @@
         view: null,
         model: null,
         messageList: null,
-        init: function (view,model) {
+        init: function (view, model) {
             this.view = view
             this.model = model
-            this.model.init() 
+
+            this.model.init()
             this.messageList = view.querySelector('#messageList')
             this.form = view.querySelector('form')
             this.loadMessages()
             this.bindEvents()
         },
-        
+
         loadMessages: function () {
             this.model.fetch().then(
-                    (messages) => {
-                        let array = messages.map((item) => item.attributes)
-                        array.forEach((item) => {
-                            let li = document.createElement('li')
-                            li.innerText = `${item.name}：${item.content}`
-                            this.messageList.appendChild(li)
-                        })
+                (messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}：${item.content}`
+                        this.messageList.appendChild(li)
+                        console.log('meiwenti')
                     })
+                })
         },
         bindEvents: function () {
             this.form.addEventListener('submit', function (e) {
                 e.preventDefault()
-                this.saveMessage()
+                controller.saveMessage()
             })
         },
         saveMessage: function () {
             let myForm = this.form
             let content = myForm.querySelector('input[name=content]').value
             let name = myForm.querySelector('input[name=name]').value
-            this.model.save(name,content).then(function (object) {
+            this.model.save(name, content).then(function (object) {
                 let li = document.createElement('li')
                 li.innerText = `${object.attributes.name}：${object.attributes.content}`
                 let messageList = document.querySelector('#messageList')
                 messageList.appendChild(li)
                 myForm.querySelector('input[name=content]').value = ''
+                console.log('1')
             })
         }
     }
-    controller.init(view,model)
+    controller.init(view, model)
 }.call()
 
 
